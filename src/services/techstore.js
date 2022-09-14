80700e5f25e88d34837633032539b062184294ff
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-09-14 15:37:21 -0300

    adicionado variaveis de ambiente

---
+++ src/services/techstore.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+
+function createHeaders() {
+    const auth = JSON.parse(localStorage.getItem("mywallet"))
+    const config = {
+        headers: {
+            Authorization: `Bearer ${auth.token}`
+        }
+    };
+
+    return config;
+}
